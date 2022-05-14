c54519a1fdc1dfcde856358bddb1eeb73e657886
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-05-14 16:58:32 +0900

    공통 컴포넌트 (레이아웃 위주)

---
+++ m-to-f/src/components/common/MoveBtn.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import styled from "styled-components"
+
+interface IMove {
+    text: string
+}
+const MoveBtn = ({text} : IMove) => {
+    return (
+        <StyledWrapper>
+            <StyledLink>{text}</StyledLink>
+        </StyledWrapper>
+    )
+}
+
+const StyledWrapper = styled.section`
+
+margin: 1rem 0.5rem;
+text-align: right;
+
+`
+
+const StyledLink = styled.a`
+font-weight: bold;
+border-bottom: 2px solid black;
+padding-bottom: 0.75rem;
+&:after {
+    content: '→';
+    font-size: 1.2rem;
+    display: relative;
+    
+    margin-left: 1rem;
+}
+`
+
+export {
+    MoveBtn
+}
