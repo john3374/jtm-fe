1bb486aee2c4d57bf04a6a423fe711db7e648fdb
Choi33 <choi333ys@gmail.com>
2022-05-14 20:00:07 +0900

    embody kakao login

---
+++ m-to-f/src/components/kakao/InputEmail.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+function InputEmail(){
+
+    return (
+        <div>
+            <h1>이메일 입력 화면 구현 예정</h1>
+        </div>
+    )
+}
+
+export default InputEmail;
