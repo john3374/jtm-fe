ef615d7ed8a59c1c9934cc1c99c0fbaebe5a7965
choi33 <choi33ys@gmail.com>
2022-06-04 11:52:35 +0900

    router component

--- m-to-f/src/Router.tsx
+++ m-to-f/src/Router.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
-
-import EnvConfig from './components/config/EnvConfig';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
+import EnvConfig from './components/config/EnvConfig';
 
 import GoToLogin from './components/GoToLogin';
+import SignUp from './components/sign_up/SignUp';
 import KakaoLogin from './components/kakao/KakaoLogin';
 import InputEmail from './components/kakao/InputEmail';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from './components/layout/AppLayout';
 import { NamePaper } from './components/create_paper/NamePaper';
 import Theme from './components/create_paper/Theme';
-import PaperMain from './components/Paper_main/PaperMain';
-import SignUp from './components/sign_up/SignUp';
+import PaperMain from './components/create_paper/PaperMain';
 
 const Router = () => {
   const [REST_API, set_REST_API] = useState<string>('');
@@ -35,7 +34,6 @@ const Router = () => {
         console.log(err);
       });
   }, []);
-  console.log(EnvConfig.REST_API);
 
   return (
     <BrowserRouter>
@@ -43,15 +41,15 @@ const Router = () => {
         <Routes>
           <Route element={<AppLayout />}>
             <Route path="/" element={<GoToLogin api={REST_API} />} />
-            <Route path="/inputEmail" element={<InputEmail />} />
-            <Route path="/createPaper" element={<NamePaper />} />
-            <Route path="/paper" element={<PaperMain />} />
             <Route path="/signUp" element={<SignUp />} />
-            <Route path="/selectTheme" element={<Theme />} />
             <Route
               path="/oauth/kakao/callback"
               element={<KakaoLogin api={REST_API} client={CLIENT_SECRET} />}
             />
+            <Route path="/inputEmail" element={<InputEmail />} />
+            <Route path="/createPaper" element={<PaperMain />} />
+            <Route path="/createPaper/decideName" element={<NamePaper />} />
+            <Route path="/createPaper/selectTheme" element={<Theme />} />
           </Route>
         </Routes>
       ) : (
