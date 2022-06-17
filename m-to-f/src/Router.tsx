ab4f210a082e348082acd21213f3b41b8ecbe0a2
YeSeul-0w0 <choi333ys@gmail.com>
2022-06-17 21:52:28 +0900

    connection login

--- m-to-f/src/Router.tsx
+++ m-to-f/src/Router.tsx
@@ -6,11 +6,11 @@ import EnvConfig from './components/config/EnvConfig';
 import GoToLogin from './components/GoToLogin';
 import SignUp from './components/sign_up/SignUp';
 import KakaoLogin from './components/kakao/KakaoLogin';
-import InputEmail from './components/kakao/InputEmail';
 import AppLayout from './components/layout/AppLayout';
 import { NamePaper } from './components/create_paper/NamePaper';
 import Theme from './components/create_paper/Theme';
 import PaperMain from './components/create_paper/PaperMain';
+import LoginEmail from './components/login/LoginEmail';
 
 const Router = () => {
   const [REST_API, set_REST_API] = useState<string>('');
@@ -41,12 +41,12 @@ const Router = () => {
         <Routes>
           <Route element={<AppLayout />}>
             <Route path="/" element={<GoToLogin api={REST_API} />} />
-            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/login" element={<LoginEmail />} />
+            <Route path="/login/signUp" element={<SignUp />} />
             <Route
               path="/oauth/kakao/callback"
               element={<KakaoLogin api={REST_API} client={CLIENT_SECRET} />}
             />
-            <Route path="/inputEmail" element={<InputEmail />} />
             <Route path="/createPaper" element={<PaperMain />} />
             <Route path="/createPaper/decideName" element={<NamePaper />} />
             <Route path="/createPaper/selectTheme" element={<Theme />} />
