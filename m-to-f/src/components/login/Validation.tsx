fc083b7dcc348c1dd88949724f899bef88489203
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-07-02 23:52:38 +0900

    feat : login 기능 구현

---
+++ m-to-f/src/components/login/Validation.tsx
@@ -0,0 +1,6 @@
+export const isEmail = (email: string) => {
+  const emailRegex =
+    /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+  return emailRegex.test(email);
+};
