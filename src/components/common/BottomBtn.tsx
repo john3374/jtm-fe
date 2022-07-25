70e90f2974dee524d48b50bff5ef7d4cb2fcf1a7
YeSeul-0w0 <choi333ys@gmail.com>
2022-07-25 20:43:52 +0900

    solve button bug

--- src/components/common/BottomBtn.tsx
+++ src/components/common/BottomBtn.tsx
@@ -22,24 +22,20 @@ const BottomBtn = ({ text, onclick, disabled, fixed }: IBottomBtn) => {
 };
 
 const StyledBtn = styled.button<IBtnStyle>`
-  border: none;
-  bottom: 0;
   border-radius: 12px;
-  text-align: center;
-  margin: 1.25rem;
+  border: none;
+  width: 100%;
+  background: ${props => (props.disabled ? `#666666` : 'black')};
   padding: 1rem 0;
-
-  background: ${props => (props.disabled ? 'darkgray' : 'black')};
+  margin-top: 0.5rem;
+  text-align: center;
   color: white;
   font-size: 1.2rem;
   position: ${props => (props.fixed ? 'fixed' : 'unset')};
   ${props => (props.fixed ? 'width : 343px' : '')};
-  bottom: 16px;
-  left: 50%;
-  transform: translateX(-50%);
+  bottom: 0;
   // validation 통과한 경우에만 hover
   ${props => (props.disabled ? '' : '&:hover { background: #666666 }')}
-  border-radius: 12px;
 `;
 
 export default BottomBtn;
