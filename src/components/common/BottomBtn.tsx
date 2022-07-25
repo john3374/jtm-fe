b2c8d6282b98366bc4354261250849b4e4d3db9b
titmousu <88585464+bokumbob@users.noreply.github.com>
2022-07-25 11:36:05 +0900

    fix

--- src/components/common/BottomBtn.tsx
+++ src/components/common/BottomBtn.tsx
@@ -21,14 +21,7 @@ const BottomBtn = ({ text, onclick, disabled, fixed }: IBottomBtn) => {
   );
 };
 
-<<<<<<< HEAD
 const StyledBtn = styled.button<IBtnStyle>`
-=======
-const StyledBtn = styled.button`
-  width: 85%;
-  bottom: 0;
-
->>>>>>> main
   border: none;
   border-radius: 12px;
 
@@ -38,16 +31,11 @@ const StyledBtn = styled.button`
   background: ${props => (props.disabled ? 'darkgray' : 'black')};
   color: white;
   font-size: 1.2rem;
-<<<<<<< HEAD
   position: ${props => (props.fixed ? 'fixed' : 'unset')};
   ${props => (props.fixed ? 'width : 343px' : '')};
   bottom: 16px;
   left: 50%;
   transform: translateX(-50%);
-=======
-  text-align: center;
-
->>>>>>> main
   // validation 통과한 경우에만 hover
   ${props => (props.disabled ? '' : '&:hover { background: red }')}
   border-radius: 12px;
