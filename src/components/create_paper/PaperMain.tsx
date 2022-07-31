2779b42225604aedff69d930509e1c58582018ee
YeSeul-0w0 <choi333ys@gmail.com>
2022-07-31 12:48:10 +0900

    remove code

--- src/components/create_paper/PaperMain.tsx
+++ src/components/create_paper/PaperMain.tsx
@@ -6,7 +6,6 @@ import { IUser } from '@src/interfaces/ILogin';
 import FeedHeader from '../common/FeedHeader';
 import BottomBtn from '../common/BottomBtn';
 import PaperList from '../paper_view/PaperList';
-// import FloatingButton from '../common/FloatingButton';
 
 const Option = styled.div`
   width: 48px;
@@ -41,7 +40,6 @@ const ViewPapers = (user: IUser) => {
   if (user.email) userEmail = user.email;
   return (
     <main style={{ overflow: 'scroll' }}>
-      <FloatingButton />
       <h1
         style={{
           fontSize: '20px',
