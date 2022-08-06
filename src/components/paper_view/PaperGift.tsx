e73aa35d2829bedec7711791d4ea70a9c9c8094e
YeSeul-0w0 <choi333ys@gmail.com>
2022-08-06 23:49:31 +0900

    complete paper gift function (test need)

--- src/components/paper_view/PaperGift.tsx
+++ src/components/paper_view/PaperGift.tsx
@@ -6,6 +6,7 @@ import BottomBtn from '../common/BottomBtn';
 import axios from 'axios';
 import EnvConfig from '../../config/EnvConfig';
 import { useAuthState } from '../../../src/context';
+import Modal from '../common/Modal';
 
 function PaperGift() {
   const { user, token } = useAuthState();
@@ -13,19 +14,59 @@ function PaperGift() {
 
   const [selectPaperId, setSelectPaperId] = useState<number>(0);
   const [paperList, setPaperList] = useState<any>([]);
+  const [emailVerify, setEmailVerify] = useState<string>();
+  const [giftEmail, setGiftEmail] = useState<string>();
+  const [onModal, setOnModal] = useState<boolean>(false);
+  const [info, setInfo] = useState<string>('');
 
   const loadPaperList = async () => {
     try {
-      const response = await axios({
+      const getResponse = await axios({
         method: 'get',
         headers: {
           'User-Email': `${userEmail}`,
         },
         url: `${EnvConfig.CREATE_PAPER}`,
       });
-      setPaperList(response.data.paper);
+      setPaperList(getResponse.data.paper);
     } catch (e) {
       console.log(e);
+    }
+  };
+
+  const handleDropBox = (e: any) => {
+    setSelectPaperId(e.target.value);
+  };
+
+  const checkEmail = async () => {
+    if (selectPaperId !== 0) {
+      if (giftEmail === emailVerify) {
+        try {
+          const putResponse = await axios({
+            method: 'put',
+            url: `${EnvConfig.CREATE_PAPER}/gift/${selectPaperId}`,
+            data: {
+              user: {
+                email: `${userEmail}`,
+              },
+              recipient: {
+                email: `${giftEmail}`,
+              },
+            },
+          });
+          setOnModal(true);
+          setInfo('페이퍼가 선물 되었습니다!');
+        } catch (e) {
+          setOnModal(true);
+          setInfo('이메일이 동일하지 않습니다.');
+        }
+      } else {
+        setOnModal(true);
+        setInfo('존재하지 않는 이메일 입니다.');
+      }
+    } else {
+      setOnModal(true);
+      setInfo('페이퍼를 선택해주십시오.');
     }
   };
 
@@ -33,15 +74,17 @@ function PaperGift() {
     loadPaperList();
   }, []);
 
-  const handleDropBox = (e: any) => {
-    setSelectPaperId(e.target.value);
-  };
-
-  // console.log('id', selectPaperId);
-
   return (
     <>
       <Header pageNm="롤링페이퍼 만들기" to="/createPaper" />
+      {onModal ? (
+        <Modal
+          info={info}
+          confirm={false}
+          onModal={onModal}
+          setOnModal={setOnModal}
+        />
+      ) : null}
       <main>
         <Wrapper>
           <StyledLabel> 롤링 페이퍼를 선택해주세요.</StyledLabel>
@@ -50,11 +93,13 @@ function PaperGift() {
               선택해주세요
             </option>
             {paperList.map((el: any) => {
-              return (
-                <option key={el.paperId} value={el.paperId}>
-                  {el.paperTitle}
-                </option>
-              );
+              if (el.giftyn === 'N') {
+                return (
+                  <option key={el.paperId} value={el.paperId}>
+                    {el.paperTitle}
+                  </option>
+                );
+              }
             })}
           </DropDown>
         </Wrapper>
@@ -63,15 +108,17 @@ function PaperGift() {
           htmlFor="giftEmail"
           background="white"
           border="1px solid black"
+          onChange={(e: any) => setGiftEmail(e.target.value)}
         />
         <TextInput
           title="이메일을 한 번 더 입력해주세요."
           htmlFor="giftEmail"
           background="white"
           border="1px solid black"
+          onChange={(e: any) => setEmailVerify(e.target.value)}
         />
       </main>
-      <BottomBtn text="다음" />
+      <BottomBtn text="다음" onclick={checkEmail} />
     </>
   );
 }
